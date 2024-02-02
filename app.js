8222a9ca1bcf0bd0e5f2ae840ad19e8d1b75bb4f
Marcelo Magario <marcelo.magario@mzgroup.com>
2024-02-01 22:09:44 -0300

    rm SEQUELIZE and create TB Praia and Sessao Fotografo

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// Inicializando o Express e o Sequelize
 const express = require('express');
 const path = require('path');
-const Sequelize = require('sequelize');
+const { Pool } = require('pg');
 const app = express();
 
 app.use(express.json());
@@ -10,75 +9,23 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const sequelize = new Sequelize('postgres', 'postgres', 'pass', {
+const pool = new Pool({
+  user: 'postgres',
   host: 'localhost',
+  database: 'postgres',
+  password: 'pass',
   port: 5432,
-  dialect: 'postgres',
-  logging: false
 });
-
-// Testando a conexão
-sequelize.authenticate()
-  .then(() => {
-    console.log('Conexão estabelecida com sucesso.');
-  })
-  .catch(err => {
-    console.error('Não foi possível conectar ao banco de dados:', err);
-  });
-
-// Definindo os modelos
-const tbfotografo = sequelize.define('fotografo', {
-  nome: Sequelize.STRING,
-  senha: Sequelize.STRING,
-  picture: Sequelize.STRING,
-  instagram: Sequelize.STRING,
-  whatsapp: Sequelize.STRING,
-  email: Sequelize.STRING,
-  link: Sequelize.STRING
-}, {
-  freezeTableName: true
-});
-
-// Definindo o modelo Praia
-const Praia = sequelize.define('praia', {
-  nome_praia: Sequelize.STRING,
-  localizacao: Sequelize.GEOMETRY('POINT'),
-  direcao_ideal_swell: Sequelize.STRING,
-  direcao_ideal_vento: Sequelize.STRING
-}, {
-  freezeTableName: true
-});
-
-// Definindo o modelo SessaoFotografo
-const SessaoFotografo = sequelize.define('sessao_fotografo', {
-  data: Sequelize.DATE,
-  hora_inicial: Sequelize.TIME,
-  hora_final: Sequelize.TIME
-}, {
-  freezeTableName: true
-});
-
-// Definindo as relações
-tbfotografo.hasMany(SessaoFotografo, { foreignKey: 'fotografoId' });
-SessaoFotografo.belongsTo(tbfotografo, { foreignKey: 'fotografoId' });
-
-Praia.hasMany(SessaoFotografo, { foreignKey: 'praiaId' });
-SessaoFotografo.belongsTo(Praia, { foreignKey: 'praiaId' });
-
-// Sincronizando os modelos com o banco de dados
-sequelize.sync();
-
 
 app.get('/fotografo/cadastro', (req, res) => {
   res.render('cadastro');
 });
 
-
 app.post('/fotografo/cadastro', async (req, res) => {
   const { nome, senha, picture, instagram, whatsapp, email, link } = req.body;
   try {
-    const novoFotografo = await tbfotografo.create({ nome, senha, picture, instagram, whatsapp, email, link });
-    res.json(novoFotografo);
+    const result = await pool.query('INSERT INTO fotografo(nome, senha, picture, instagram, whatsapp, email, link) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *', [nome, senha, picture, instagram, whatsapp, email, link]);
+    res.json(result.rows[0]);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Não foi possível salvar o fotógrafo' });
@@ -90,14 +37,9 @@ app.put('/fotografo/cadastro/:id', async (req, res) => {
   const { nome, senha, picture, instagram, whatsapp, email, link } = req.body;
   console.log('Rota fotografo - PUT');
   try {
-    const fotografo = await tbfotografo.findByPk(id);
-    if (fotografo) {
-      await tbfotografo.update({ nome, senha, picture, instagram, whatsapp, email, link }, {
-        where: {
-          id: id
-        }
-      });
-      res.json(fotografo);
+    const result = await pool.query('UPDATE fotografo SET nome=$1, senha=$2, picture=$3, instagram=$4, whatsapp=$5, email=$6, link=$7 WHERE id=$8 RETURNING *', [nome, senha, picture, instagram, whatsapp, email, link, id]);
+    if (result.rowCount > 0) {
+      res.json(result.rows[0]);
     } else {
       res.status(404).json({ error: 'Fotógrafo não encontrado' });
     }
@@ -111,10 +53,9 @@ app.delete('/fotografo/cadastro/:id', async (req, res) => {
   const { id } = req.params;
   console.log('Rota fotografo - DELETE');
   try {
-    const fotografo = await tbfotografo.findByPk(id);
-    if (fotografo) {
-      await fotografo.destroy();
-      res.json(fotografo);
+    const result = await pool.query('DELETE FROM fotografo WHERE id=$1 RETURNING *', [id]);
+    if (result.rowCount > 0) {
+      res.json(result.rows[0]);
     } else {
       res.status(404).json({ error: 'Fotógrafo não encontrado' });
     }
@@ -124,22 +65,19 @@ app.delete('/fotografo/cadastro/:id', async (req, res) => {
   }
 });
 
-// Rota para listar todos os fotografos
 app.get('/fotografo', async (req, res) => {
   try {
-    const listaFotografos = await tbfotografo.findAll();
-    res.json(listaFotografos);
-    // res.render('lista', { fotografo: listaFotografos });
+    const result = await pool.query('SELECT * FROM fotografo');
+    res.json(result.rows);
   } catch (err) {
     console.error('Erro ao buscar fotografos:', err);
     res.status(500).json({ error: 'Não foi possível buscar os fotografos' });
   }
-}
-);
+});
 
 
 // Iniciando o servidor
-app.listen(3000, async () => {
-  await sequelize.sync();
+
+app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
